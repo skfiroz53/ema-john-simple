467069b91898c6d2a67f3f4eae812f7133c79333
skfiroz <skfiroz5305@gmail.com>
2024-01-09 12:44:07 +0800

    Added my project

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
  import React from 'react';
  import './Header.css'
  import logo from '../../images/Logo.svg';
-import { Link } from 'react-router-dom';
+ import { Link } from 'react-router-dom';
 
  const Header = () => {
     return (
@@ -40,23 +40,3 @@ import { Link } from 'react-router-dom';
 
 
 
-//  =======================pactice start ===========================
-
-// import React from 'react';
-// import css from './Header.css'
-// import Logo from '../../images/Logo.svg';
-// const Header = () => {
-//     return (
-//         <nav className='header'>
-//              <img src={Logo} alt="" />
-//              <div>
-//                 <a href="/Order">Order</a>
-//                 <a href="/Order Review">Order Review</a>
-//                 <a href="/Manage Inventory">Manage Inventory</a>
-//                 <a href="/Login">Login</a>
-//              </div>
-//         </nav>
-//     );
-// };
-
-// export default Header;
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,74 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import './Shop.css';
-// import Product from '../Product/Product';
-// import Cart from '../Cart/Cart';
-// import { addToDb, getShoppingCart } from '../../utilities/fakedb';
-
-// const Shop = () => {
-//     const [products,setProducts] = useState([]);
-//     const [cart,setCart] = useState([])
-//     useEffect(() =>{
-//         fetch('products.json')
-//         .then(res =>res.json())
-//         .then(data =>setProducts(data))
-//     },[])
-      
-//    useEffect( () =>{
-//     const storedCart = getShoppingCart();
-//     const savedCart = [];
-//     // step 1: get id of the addedProduct
-//     for(const id in storedCart){
-//         // step 2: get product from products state by using id
-//         const addedProduct = products.find(product => product.id === id)
-//         if(addedProduct){
-//         //     // step 3: add quantity
-//             const quantity = storedCart[id];
-//             addedProduct.quantity = quantity;
-//         //     // step 4: add the added product to the saved cart
-//             savedCart.push(addedProduct);
-//         }
-//         console.log('added Product', addedProduct)
-//     }
-//     // step 5: set the cart
-//     setCart(savedCart);
-//     }, [products])
-
-
-//         const handleAddToCart = (product)=>{
-//             const newCart = [...cart,product];
-//             setCart(newCart);
-//             addToDb(product.id)
-//         }
-
-//     return (
-//         <div className='shop-container'>
-//             <div className="products-container">
-//                 {
-//                     products.map(product =><Product
-//                     key = {product.id}
-//                     product ={product}
-//                     handleAddToCart = {handleAddToCart}
-//                     ></Product>)
-//                 }
-//             </div>
-//              <div className="cart-container">
-//                  <Cart cart ={cart}></Cart>
-//              </div>
-//         </div>
-
-//     );
-// };
-
-// export default Shop;
-
-
-
-// phero 51 start
-
-
-
-
-
 import React, { useEffect, useState } from 'react';
 import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
@@ -157,22 +86,6 @@ export default Shop;
 
 
 
-// ====================================amr code start==================================
-//     useEffect( () =>{
-//          const storedCart = getShoppingCart();
-//          // step 1: get id
-//         for (const id in storedCart){
-//             // step 2: get the product by using id
-//         //  const addedProduct = products.find(product => product.id === id);
-//          const addedProduct = products.find(product =>product.id === id);
-//          console.log(addedProduct);
-//          //  step 3: get quantity of the product
-//         //   const quantity = storedCart[id];
-//         //   addedProduct.quantity = quantity
-//         // console.log(addedProduct)
-//    }
-        
-//     },[products])
 
 
-// =================================== amr code end =====================================
+
